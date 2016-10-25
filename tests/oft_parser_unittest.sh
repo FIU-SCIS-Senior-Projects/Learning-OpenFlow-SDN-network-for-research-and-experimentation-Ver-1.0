74eb2d2a851e5dc79c9a6f6a08ea6c80e93165fb
Jahkell Lazarre <jlaza020@fiu.edu>
2016-10-25 12:36:47 -0400

    Update oft_parser_unittest.sh

--- tests/oft_parser_unittest.sh
+++ tests/oft_parser_unittest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+PYTHON_SCRIPT_NAME="oft_parser.py"
 SAMPLE_OUTPUT_DIR="dat/"
 JSON_PATH="sample_log.json"
 
 echo "Running Unit Test for OFTParser"
-python $(SAMPLE_OUTPUT_DIR) $(JSON_PATH)
+python $PYTHON_SCRIPT_NAME $SAMPLE_OUTPUT_DIR $JSON_PATH
+echo "Done! Check output in $JSON_PATH"
