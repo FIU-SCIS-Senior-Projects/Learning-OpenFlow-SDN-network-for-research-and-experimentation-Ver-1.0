9ed1fcc4fbd60c742ddd0e3cb6c6087050b8ac51
Steven Lyons <slyon001@fiu.edu>
2016-11-11 11:58:07 -0500

    Removed verbose as global variable. New comments. New config and profile checks.

--- ryu/ryu_compatibility_test.py
+++ ryu/ryu_compatibility_test.py
@@ -57,15 +57,13 @@ def fatal_error(msg):
     # TODO consider changing from exit to raising a custom exception (RyuCompError?)
     sys.exit(1)
 
-verbose = False
-
 """
     verbose_msg:
     Outputs message when verbose option is on.
-    msg: Message to output
+    msg: Message to output.
+    verbose: If True, output the message.
 """
-def verbose_msg(msg):
-    global verbose
+def verbose_msg(msg, verbose):
     if verbose:
         print msg
 
@@ -73,9 +71,10 @@ def verbose_msg(msg):
     load_config:
     Load a config file (JSON formatted).
     config_path: 'path/to/config'
+    verbose: If True, output verbose messages.
     return: config dictionary object
 """
-def load_config(config_path):
+def load_config(config_path, verbose=False):
     # default config dictionary object
     config = {
         'directory': '{}/ryu_results'.format(os.environ['HOME']),
@@ -83,63 +82,95 @@ def load_config(config_path):
 
     # if doesn't exist, create the default config file
     if not os.path.exists(config_path):
-        verbose_msg('Config: Config file {} not found. Generating default config at location.'.format(config_path))
+        verbose_msg('Config: Config file {} not found. Generating default config at location.'\
+		            .format(config_path), verbose)
         with open(config_path, 'w') as config_file:
             json.dump(config, config_file, sort_keys=True)
 
-    verbose_msg('Config: Loading config file {}.'.format(config_path))
+    verbose_msg('Config: Loading config file {}.'.format(config_path), verbose)
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
 
-    # TODO check if any missing, if so, use default settings for missing keys
-    # TODO update as less/more keys are used for config
+    check_config(config)
 
-    verbose_msg('Config: {} loaded successfully.'.format(config_path))
+    verbose_msg('Config: {} loaded successfully.'.format(config_path), verbose)
     return config
 
 """
-   load_profile:
-   Load a profile file (JSON formatted).
-   profile_path: 'path/to/profile'
-   return: profile dictionary object
+    check_config:
+    Check if a configuration is valid.
+    config: Configuration to check.
 """
-def load_profile(profile_path):
+def check_config(config):
+    # Check if any keys missing, if so, throw error
+    check_list = ['directory']
+    missing = []
+
+    for check in check_list:
+        if not check in config:
+            missing.append(check)
+
+    if len(missing) > 0
+        msg = 'Config: Missing necessary keys.'
+        for miss in missing:
+            msg += '\nMissing {}'.format(miss)
+        fatal_error(msg)
+    
+
+"""
+    load_profile:
+    Load a profile file (JSON formatted).
+    profile_path: 'path/to/profile'
+    verbose: If True, output verbose messages.
+    return: profile dictionary object
+"""
+def load_profile(profile_path, verbose=False):
     if not os.path.exists(profile_path):
         fatal_error('Profile file {} not found.'.format(profile_path))
 
-    verbose_msg('Profile: Loading profile file {}.'.format(profile_path))
+    verbose_msg('Profile: Loading profile file {}.'.format(profile_path), verbose)
     with open(profile_path, 'r') as profile_file:
         profile = json.load(profile_file)
 
-    # TODO: update as less/more keys are used for profile
-    profile_keys = ['model', 'of_version', 'compatibility']
-    missing_keys = []
-    for key in profile_keys:
-        if key not in profile:
-            missing_keys.append(key)
-    if len(missing_keys) > 0:
-        msg = 'Profile file {} is missing necessary keys.'.format(profile_path)
-        for key in missing_keys:
-            msg += '\nMissing {}'.format(key)
-        fatal_error(msg)
+    check_profile(profile)
 
-    verbose_msg('Profile: {} loaded successfully'.format(profile_path))
+    verbose_msg('Profile: {} loaded successfully'.format(profile_path), verbose)
     return profile
+
+"""
+    check_profile:
+    Check if a profile is valid.
+    profile: Profile to check.
+"""
+def check_profile(profile):
+    # Check if any keys missing, if so, throw error
+    check_list = ['name', 'of_version', 'compatibility']
+    missing = []
         
+    for check in check_list:
+        if check not in profile:
+            missing.append(check)
+
+    if len(missing) > 0:
+        msg = 'Profile: Missing necessary keys.'
+        for miss in missing:
+            msg += '\nMissing {}'.format(miss)
+        fatal_error(msg)
 
 """
     create_dir:
     Function to create a directory if it doesn't exist.
     Throws OSError or crashes on failure.
     dir_to_create: 'path/to/directory'
+    verbose: If True, output verbose messages.
 """
-def create_dir(dir_to_create):
+def create_dir(dir_to_create, verbose=False):
     if not os.path.exists(dir_to_create):
-        verbose_msg('Misc: Creating directory {}'.format(dir_to_create))
+        verbose_msg('Misc: Creating directory {}'.format(dir_to_create), verbose)
         os.makedirs(dir_to_create)
     elif not os.path.isdir(dir_to_create):
         fatal_error('{} already exists, but is not a directory.'\
-                .format(dir_to_create))
+                    .format(dir_to_create))
 
 """
     get_results:
@@ -149,12 +180,16 @@ def create_dir(dir_to_create):
     profile: Profile dictionary object
     backup: Backup old result files (optional)
     force_test: Run Ryu tests regardless of existing results (optional)
+    verbose: If True, output verbose messages.
     NOTE: Does not catch exceptions. Failure is desired.
     NOTE: Can also end program should Ryu testing run into errors.
 """
 def get_results(config, profile, backup=False, force_test=False,
-                tester_dpid=1, target_dpid=2):
-    switch_model = profile['model']
+                tester_dpid=1, target_dpid=2, verbose=False):
+    check_config(config)
+    check_profile(profile)
+
+    switch_model = profile['name']
     of_version = profile['of_version']
 
     create_dir(config['directory'])
@@ -163,18 +198,19 @@ def get_results(config, profile, backup=False, force_test=False,
 
     switch_json = '{}/{}_{}.json'.format(switch_dir, switch_model, of_version)
     switch_csv = '{}/{}_{}.csv'.format(switch_dir, switch_model, of_version)
+
     if backup:
         if os.path.exists(switch_json):
             os.rename(switch_json, '{}.bak'.format(switch_json))
-            verbose_msg('Misc: Backed up {0} as {0}.bak.'.format(switch_json))
+            verbose_msg('Misc: Backed up {0} as {0}.bak.'.format(switch_json), verbose)
         if os.path.exists(switch_csv):
             os.rename(switch_csv, '{}.bak'.format(switch_csv))
-            verbose_msg('Misc: Backed up {0} as {0}.bak.'.format(switch_csv))
+            verbose_msg('Misc: Backed up {0} as {0}.bak.'.format(switch_csv), verbose)
 
     if force_test or not os.path.exists(switch_json) or not os.path.exists(switch_csv):
         if force_test or not os.path.exists(switch_json):
             verbose_msg('Main: Testing {} with Ryu for {}.'\
-                        .format(switch_model, of_version))
+                        .format(switch_model, of_version), verbose)
 
             """
                 FOR TESTING WITHOUT RUNNING FULL RYU TESTS
@@ -183,8 +219,6 @@ def get_results(config, profile, backup=False, force_test=False,
             error = []
 
             """
-            # TODO: add some customizability options from config file
-            # run tests and convert to json
             ryu_switch_test_dir = 'ryu/ryu/tests/switch'
             ryu_command = 'ryu-manager --test-switch-dir {0}/{1} --test-switch-tester {2} --test-switch-target {3} {0}/tester.py'\
                             .format(ryu_switch_test_dir, of_version, tester_dpid, target_dpid)
@@ -194,18 +228,18 @@ def get_results(config, profile, backup=False, force_test=False,
                                         stderr=subprocess.PIPE)
             results, error = ryu_test.communicate()
             
-            # TODO: actually write output to file
+            # TODO: actually write raw Ryu output to file?
 
-            verbose_msg('Main: Testing complete.')
+            verbose_msg('Main: Testing complete.', verbose)
 
             if len(error) > 0:
                 fatal_error('Ryu testing encountered error(s) when running:\
                              \n{}'.format(error))
 
             results = results.split('\n')
-            ryu_to_json(results, switch_json)
-    
-        json_to_csv(switch_json, switch_csv)
+            ryu_to_json(results, switch_json, verbose)
+   
+        json_to_csv(switch_json, switch_csv, verbose)
 
 """
     ryu_to_json:
@@ -213,9 +247,10 @@ def get_results(config, profile, backup=False, force_test=False,
       JSON format for engineers to look at and use.
     ryu_results: Array of lines representing the output of Ryu test
     json_path: path/to/json/file
+    verbose: If True, output verbose messages.
 """
-def ryu_to_json(ryu_results, json_path):
-    verbose_msg('Main: Saving detailed results to {}.'.format(json_path))
+def ryu_to_json(ryu_results, json_path, verbose=False):
+    verbose_msg('Main: Saving detailed results to {}.'.format(json_path), verbose)
 
     results = {}
     errors = []
@@ -269,7 +304,7 @@ def ryu_to_json(ryu_results, json_path):
     with open(json_path, 'w') as json_file:
         json.dump(results, json_file, sort_keys=True, ensure_ascii=True)
 
-    verbose_msg('Main: {} written successfully.'.format(json_path))
+    verbose_msg('Main: {} written successfully.'.format(json_path), verbose)
 
 """
     json_to_csv:
@@ -277,24 +312,26 @@ def ryu_to_json(ryu_results, json_path):
       simple CSV file with simple results for easy exporting.
     json_path: path/to/json/file
     csv_path: path/to/csv/file
+    verbose: If True, output verbose messages.
 """
-def json_to_csv(json_path, csv_path):
+def json_to_csv(json_path, csv_path, verbose=False):
     verbose_msg('Main: Converting detailed {} to simplified CSV file {}.'\
-                .format(json_path, csv_path))
-    with open(json_path, 'r') as json_file:
+                .format(json_path, csv_path), verbose)
+    with open(json_path, 'r') as json_file,
+         open(csv_path, 'wb') as csv_file:
         switch_json = json.load(json_file)
-        with open(csv_path, 'wb') as csvfile:
-            csv_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-            for test_general in sorted(switch_json):
-                result = None
-                for test_specific in switch_json[test_general]:
-                    test_result = switch_json[test_general][test_specific]['result']
-                    if not result:
-                        result = test_result
-                    elif result != test_result:
-                        result = 'DIFF'
-                csv_writer.writerow([test_general, result])
-    verbose_msg('Main: {} written successfully'.format(csv_path))
+        csv_writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        for test_general in sorted(switch_json):
+            # all specific tests need to have the same result to make a conclusion
+            result = None
+            for test_specific in switch_json[test_general]:
+                test_result = switch_json[test_general][test_specific]['result']
+                if not result:
+                    result = test_result
+                elif result != test_result:
+                    result = 'DIFF'
+            csv_writer.writerow([test_general, result])
+    verbose_msg('Main: {} written successfully'.format(csv_path), verbose)
 
 
 """
@@ -303,8 +340,9 @@ def json_to_csv(json_path, csv_path):
       results of tests specified in profile. (goes to STDOUT)
     config: Configuration dictionary object
     prfile: Profile dictionary object
+    verbose: If True, output verbose messages.
 """
-def judge_results(config, profile):
+def judge_results(config, profile, verbose=False):
     switch_model = profile['model']
     of_version = profile['of_version']
     switch_dir = '{}/{}'.format(config['directory'], switch_model)
@@ -313,7 +351,7 @@ def judge_results(config, profile):
     if not os.path.exists(csv_path):
         fatal_error('Cannot find CSV result file {}.'.format(csv_path))
 
-    verbose_msg('Main: Beginning judging.')
+    verbose_msg('Main: Beginning judging.', verbose)
     failures = []
     comp_list = profile['compatibility'][:]
     with open(csv_path, 'r') as csv_file:
@@ -323,12 +361,13 @@ def judge_results(config, profile):
                 if result != 'OK':
                     failures.append( (name, result) )
                 comp_list.remove(name)
+    # any compatibility failures or features without results are reported
     if len(failures) > 0 or len(comp_list) > 0:
         print '{} FAILED'.format(profile['model'])
         for name, result in failures:
-            verbose_msg('FAILED TESTS: {},{}'.format(name, result))
+            verbose_msg('FAILED TESTS: {},{}'.format(name, result), verbose)
         for name in comp_list:
-            verbose_msg('NOT FOUND: {}'.format(name))
+            verbose_msg('NOT FOUND: {}'.format(name), verbose)
     else:
         print '{} PASSED'.format(profile['model'])
 
@@ -339,9 +378,9 @@ if __name__ == '__main__':
 
     backup = False
     force_test = False
+    verbose = False
 
     # go through args
-    # TODO: port definitions? or is that in config?
     args = iter(sys.argv[1:])
     for arg in args:
         if arg == '-b':
@@ -361,12 +400,11 @@ if __name__ == '__main__':
         else:
             fatal_error('Unknown argument encountered: {}'.format(arg))
 
-    # TODO expand and/or replace to make sure everything needed it here
     if not profile_path:
         fatal_error('Missing arguments: {}'.format('Profile location'))
 
-    config = load_config(config_path)
-    profile = load_profile(profile_path)
+    config = load_config(config_path, verbose)
+    profile = load_profile(profile_path, verbose)
     get_results(config, profile, backup=backup, force_test=force_test,
-                tester_dpid=tester_dp, target_dpid=target_dp)
-    judge_results(config, profile)
+                tester_dpid=tester_dp, target_dpid=target_dp, verbose)
+    judge_results(config, profile, verbose)
