2fb9933fd8332166f6a51266e7f3546a9319a8ff
Jahkell Lazarre <jlaza020@fiu.edu>
2016-11-19 20:08:20 -0800

    Cleaned up branch

--- tests/oft_parser_unittest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PYTHON_SCRIPT_NAME="oft_parser.py"
-SAMPLE_OUTPUT_DIR="dat/"
-JSON_PATH="sample_log.json"
-
-echo "Running Unit Test for OFTParser"
-python $PYTHON_SCRIPT_NAME $SAMPLE_OUTPUT_DIR $JSON_PATH
-echo "Done! Check output in $JSON_PATH"
